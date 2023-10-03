cf5f4b867f16ff1f9adebff79b8384c678d9de88
Jarlin Andres Fonseca Bermon <jarlinandres5000@gmail.com>
2023-10-02 19:06:19 -0500

    Balanceo de carga del lado del cliente con Ribbon

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
@@ -5,9 +5,11 @@ import java.util.Collections;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
+@RibbonClient(name ="servicio-productos")
 @EnableFeignClients
 @SpringBootApplication
 @ImportAutoConfiguration({FeignAutoConfiguration.class})
