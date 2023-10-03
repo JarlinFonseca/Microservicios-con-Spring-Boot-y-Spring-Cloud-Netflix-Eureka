3fbddab0ea404faa34833aa14e9217932135b16a
Jarlin Andres Fonseca Bermon <jarlinandres5000@gmail.com>
2023-10-02 19:17:39 -0500

    Utilizando Ribbon para balanceo de carga en Feign

--- springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/models/entity/Producto.java
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/models/entity/Producto.java
@@ -13,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 
 @Entity
 @Table(name = "productos")
@@ -26,8 +27,9 @@ public class Producto implements Serializable {
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
 	private Date createAt;
+	@Transient
+	private Integer port;
 	
-	private static final long serialVersionUID = 1285454306356845809L;
 	
 	public Long getId() {
 		return id;
@@ -53,6 +55,13 @@ public class Producto implements Serializable {
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
 	}
+	public Integer getPort() {
+		return port;
+	}
+	public void setPort(Integer port) {
+		this.port = port;
+	}
 	
+	private static final long serialVersionUID = 1285454306356845809L;
 	
 }
